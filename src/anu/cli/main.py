5647b8eb97797dd05018ce428f8c236b4fc4bf0a
Ankit Kumar <hello@ankdev.me>
2020-05-16 04:33:07 +0530

    cli msg update

--- src/anu/cli/main.py
+++ src/anu/cli/main.py
@@ -8,5 +8,5 @@ from .. import __version__
 @click.group()
 @click.version_option(version=__version__)
 def main(language: str) -> None:
-    """The hypermodern Python project."""
+    """Predict the interaction parameters."""
     click.secho("Hello, World!", fg="green")
